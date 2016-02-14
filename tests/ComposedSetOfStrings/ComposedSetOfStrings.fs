edbc091417f0c812b8dde3d0d4dfd1827b75fc90
Kristian Kjems <Kristian Kjems>
2016-02-14 21:29:05 +0100

    Added '*' to split and reordered code

--- tests/ComposedSetOfStrings/ComposedSetOfStrings.fs
+++ tests/ComposedSetOfStrings/ComposedSetOfStrings.fs
@@ -1,13 +1,12 @@
 ﻿namespace ComposedSetOfStrings
-module ComposedSet =
-    open System.Text.RegularExpressions
+module ComposedSet =    
     open ComposedSet.ComposedSet
     open Common
+    open System.Text.RegularExpressions
     let parts          = ResizeArray<string>()
-    let regex          = Regex(@"("")|(\])|(\[)|(\()|(\))|(\t)|(:)|(')|(;)|(-)|(\?)|(!)|(\r)|(\n)|(,)|(\ )|(\.)|(\/)|(\@)|(_)|(\f)", RegexOptions.Compiled)
+    let regex          = Regex(@"("")|(\])|(\[)|(\()|(\))|(\t)|(:)|(')|(\*)|(;)|(-)|(\?)|(!)|(\r)|(\n)|(,)|(\ )|(\.)|(\/)|(\@)|(_)|(\f)", RegexOptions.Compiled)
     let split composed = regex.Split composed |> Array.filter (System.String.IsNullOrEmpty >> not)
     let decompose      = decomposer parts split        : string -> Decomposed<string>
     let compose        = composer parts String.build   : Decomposed<string> -> string
-    let getParts       = 
-        let getParts' (p : ResizeArray<string>) = (p |> String.concat "|") + "\n[Count:" + p.Count.ToString() + "]"
-        (fun () -> getParts' parts)
+    let getParts       = let getParts' (p : ResizeArray<string>) = (p |> String.concat "|") + "\n[Count:" + p.Count.ToString() + "]"
+                         (fun () -> getParts' parts)
