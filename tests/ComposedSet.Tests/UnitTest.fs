a3664984889fbfdbe39fe9ec6309945550c98201
Kristian Kjems <Kristian Kjems>
2016-02-17 09:26:58 +0100

    Project structure refactor

--- tests/ComposedSet.Tests/UnitTest.fs
+++ tests/ComposedSet.Tests/UnitTest.fs
@@ -1,65 +1,61 @@
-﻿module ComposedSet.Test
+﻿module ComposedSet.FsCheck
 open FsCheck
 open FsCheck.Xunit
 open ComposedSet
 open ComposedSetOfStrings
 
 type Decomposable = string
-
 let randomSequence length xs = Gen.arrayOfLength length (Gen.elements xs)
-
 let randomSequenceOfChars =
     let randomSequenceOfChars' length =
         gen{ 
             let chars = "ABCDEF./ ".ToCharArray()        
             let! size = Gen.choose (0, length)
             let body = randomSequence size chars
-            return! Gen.map (System.String : char[] -> System.String) body
+            return! Gen.map (Decomposable : char[] -> Decomposable) body
         }
     Gen.sized randomSequenceOfChars'
 
-type DecomposableGenerators = 
-    static member Decomposable() = 
-        {new Arbitrary<Decomposable>() with 
-            override x.Generator = randomSequenceOfChars
-            override x.Shrinker t = Seq.empty}
+type DecomposableGenerators = static member Decomposable() = randomSequenceOfChars |> Arb.fromGen
 
-[<Property(Arbitrary=[|typeof<DecomposableGenerators>|])>]
-let hashcode (validStrA:Decomposable) (validStrB:Decomposable) = 
-    let a = ComposedSet.decompose validStrA
-    let b = ComposedSet.decompose validStrB
-    let equalHash  = (ComposedSet.calchash a = ComposedSet.calchash b)
-    let equalValue = (a = b)
-    // Two difference values could have equal hash, but I expect from the hash function that to be extremly unlikly
-    equalHash = equalValue
+[<Arbitrary(typeof<DecomposableGenerators>)>]
+module Tests =
 
-[<Property(Arbitrary=[|typeof<DecomposableGenerators>|])>]
-let equals (validStrA : Decomposable) (validStrB : Decomposable) =
-    let a = ComposedSet.decompose validStrA
-    let b = ComposedSet.decompose validStrB            
-    (validStrA = validStrB) = (a = b)
+    [<Property>]
+    let hashcode (validStrA:Decomposable) (validStrB:Decomposable) = 
+        let a = ComposedSet.decompose validStrA
+        let b = ComposedSet.decompose validStrB
+        let equalHash  = (ComposedSet.calchash a = ComposedSet.calchash b)
+        let equalValue = (a = b)
+        // Two difference values could have equal hash, but I expect from the hash function that to be extremly unlikly
+        equalHash = equalValue
 
+    [<Property>]
+    let equals (validStrA : Decomposable) (validStrB : Decomposable) =
+        let a = ComposedSet.decompose validStrA
+        let b = ComposedSet.decompose validStrB            
+        (validStrA = validStrB) = (a = b)
 
-[<Property(Arbitrary=[|typeof<DecomposableGenerators>|])>]
-let trim (validStrA : Decomposable) (validStrB : Decomposable) =
-    let a = ComposedSet.decompose validStrA
-    let b = ComposedSet.decompose validStrB        
-    let end_trimmed = ComposedSet.trimend a b
-    let start_trimmed = ComposedSet.trimstart a end_trimmed
-    let conc = ComposedSet.concat end_trimmed start_trimmed
-    (ComposedSet.compose a) = (ComposedSet.compose conc)
+    [<Property>]
+    let trim (validStrA : Decomposable) (validStrB : Decomposable) =
+        let a = ComposedSet.decompose validStrA
+        let b = ComposedSet.decompose validStrB        
+        let end_trimmed = ComposedSet.trimend a b
+        let start_trimmed = ComposedSet.trimstart a end_trimmed
+        let conc = ComposedSet.concat end_trimmed start_trimmed
+        (ComposedSet.compose a) = (ComposedSet.compose conc)
 
-[<Property(Arbitrary=[|typeof<DecomposableGenerators>|])>]
-let concat (validStrA : Decomposable) (validStrB : Decomposable) (validStrC : Decomposable) =
-    let (++) = ComposedSet.concat
-    let a = ComposedSet.decompose validStrA
-    let b = ComposedSet.decompose validStrB
-    let c = ComposedSet.decompose validStrC
-    (ComposedSet.compose (a ++ b ++ c)) = (validStrA + validStrB + validStrC)
+    [<Property>]
+    let concat (validStrA : Decomposable) (validStrB : Decomposable) (validStrC : Decomposable) =
+        let (++) = ComposedSet.concat
+        let a = ComposedSet.decompose validStrA
+        let b = ComposedSet.decompose validStrB
+        let c = ComposedSet.decompose validStrC
+        (ComposedSet.compose (a ++ b ++ c)) = (validStrA + validStrB + validStrC)
 
 
-let config = { Config.Quick with MaxTest = 10000; Arbitrary = [typeof<DecomposableGenerators>]}
-Check.One(config, hashcode)
-Check.One(config, equals)
-Check.One(config, trim)
-Check.One(config, concat)
+let config = { Config.Quick with MaxTest = 10000; Arbitrary =[ typeof<DecomposableGenerators> ] }
+Check.One(config, Tests.hashcode)
+Check.One(config, Tests.equals)
+Check.One(config, Tests.trim)
+Check.One(config, Tests.concat)
